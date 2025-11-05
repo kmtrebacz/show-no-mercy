a2d9ab1655f182d888e55d8f3611a3b234114c42
reynevan <119664516+kmtrebacz@users.noreply.github.com>
2025-11-05 08:07:21 +0100

    add functionality to some buttons

---
+++ Scripts/UI/StartMenu.cs
@@ -0,0 +1,47 @@
+using Godot;
+using System;
+
+public partial class StartMenu : Control
+{
+	[Export]
+	private Button _startButton;
+	[Export]
+	private Button _quitButton;
+	[Export] 
+	private Panel _startPanel;
+	[Export]
+	private Button _connectButton;
+	[Export]
+	private Button _backButton;
+
+	[Export] 
+	private Panel _playPanel;
+	
+	public override void _Ready()
+	{
+		_startButton.Pressed += HandleStartButtonPressed;
+		_quitButton.Pressed += HandleQuitButtonPressed;
+		_backButton.Pressed += HandleBackButtonPressed;
+	}
+	
+	private void HandleStartButtonPressed()
+	{
+		_startPanel.SetVisible(false);
+		_playPanel.SetVisible(true);
+	}
+
+	private void HandleQuitButtonPressed()
+	{
+		GetTree().Quit();
+	}
+
+	private void HandleBackButtonPressed()
+	{
+		_startPanel.SetVisible(true);
+		_playPanel.SetVisible(false);
+	}
+	
+	public override void _Process(double delta)
+	{
+	}
+}
